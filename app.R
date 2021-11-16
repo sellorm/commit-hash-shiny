ca4f071ac9d02f1a7e3b4c2d765ee531db05f935
Mark Sellors <github@5vcc.com>
2021-11-16 09:58:46 +0000

    First commit

---
+++ app.R
@@ -0,0 +1,57 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Old Faithful Geyser Data"),
+
+    # Sidebar with a slider input for number of bins
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("bins",
+                        "Number of bins:",
+                        min = 1,
+                        max = 50,
+                        value = 30),
+            shiny::textOutput("gitRev")
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("distPlot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    })
+    output$gitRev <- renderText({
+      git_output <- system2(command = "git",
+                            args = c("rev-parse", "--short", "HEAD"),
+                            stdout = TRUE,
+                            stderr = FALSE)
+      paste0("Git commit hash: ", git_output)
+    })
+}
+
+# Run the application
+shinyApp(ui = ui, server = server)
